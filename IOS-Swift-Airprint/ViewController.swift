ff06ed64a762900db4f9b1d719999f6a0b423a32
Pooya Hatami <pooya@node-1w7jr9qro06sw04hcipbybhw2.ipv6.telus.net>
2018-07-31 18:42:49 -0700

    done

--- IOS-Swift-Airprint/ViewController.swift
+++ IOS-Swift-Airprint/ViewController.swift
@@ -10,16 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textView: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func printText(_ sender: Any) {
+        // 1  The UIPrintInteractionController presents the user interface and manages the printing.
+        let printController = UIPrintInteractionController.shared
+        // 2  The UIPrintinfo object contains information about the print job, This information is assigned to the printInfo property of the UIPrintInteractionController.
+        let printInfo = UIPrintInfo(dictionary:nil)
+        printInfo.outputType = UIPrintInfoOutputType.general
+        printInfo.jobName = "print Job"
+        printController.printInfo = printInfo
+        
+        // 3 The printing text can be formatted. here we define the insets for the printing page
+        let formatter = UIMarkupTextPrintFormatter(markupText: textView.text)
+        formatter.perPageContentInsets = UIEdgeInsets(top: 72, left: 72, bottom: 72, right: 72)
+        printController.printFormatter = formatter
+        
+        // 4 The user is presented the iPhone printing interface to select a printer and the number of copies
+        printController.present(animated: true, completionHandler: nil)
     }
-
-
+    
 }
 
